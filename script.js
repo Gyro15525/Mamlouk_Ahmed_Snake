4962f113ba3ce8f4092ce2f6f128e23fa8032119
Gyro15525 <mamloukahmed248@gmail.com>
2025-11-23 18:31:58 +0100

    jai compris comment document.addEventListener passe un objet a la fonction appelée jai cree la fonction dessiner qui dessine un rectangle vert 20x20 et reinitialise le canvas jai cree la fonction clavier qui change la direction en fonction de linput

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const canvas = document.getElementById('Canvas');
+const canvas = document.getElementById('Canvas'); // canvas 400x400
 const ctx = canvas.getContext('2d');
 
 let score = 0;
@@ -13,12 +13,43 @@ function start(){             //fonction pour start
 function pause(){           //fonction pour pause
     avance = false;
 }
-document.getElementById("start").addeventListener("click",start);  //jai associe le bouton start a la fonction start
-document.getElementById("pause").addeventListener("click",pause);  //jai associe le bouton pause a la fonction pause
+
+document.getElementById("start").addEventListener("click",start);  //jai associe le bouton start a la fonction start
+document.getElementById("pause").addEventListener("click",pause);  //jai associe le bouton pause a la fonction pause
+
+let x=0;
+let y=0;     // variables globales pour la position du snake
+let direction=0; // variable pour la direction du snake
+//la direction depend de linput du clavier
+
+function clavier(x){    //fonction pour gerer linput du clavier
+    if (x.key =='ArrowRight'){ 
+        direction=20;
+    }
+    else if (x.key =='ArrowLeft'){
+        direction=-20;
+    }
+}
+document.addEventListener('keydown', clavier); //va passer un objet qui conttient des infos sur la touche appuyee
+
+
+
+
+
+
+
+function dessiner(){
+    ctx.clearRect(0,0,400,400); //reinitalise le canvas pour effacer le snake precedent
+    ctx.fillStyle = "green";
+    ctx.fillRect(x,0,20,20); //dessine le snake 20x20 en x,y
+}
+
 
 function jeu(){
     if(avance===true){  //si le jeu nest pas en pause        
-        
+        dessiner();
+        x=x+direction;  //je deplace le snake en fonction de direction
     }
     setTimeout(jeu, 200); //je recommence la fonction
 }
+jeu();
